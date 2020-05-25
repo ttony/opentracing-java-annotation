c2c04fa6389e2db188ede3792eece1aa1dcc791d
Yiwen Ng <ttony@mjsoft.com.au>
2020-05-25 13:28:47 +0800

    remove unused code

--- src/main/java/io/opentracing/contrib/annotation/utils/ExceptionUtils.java
+++ src/main/java/io/opentracing/contrib/annotation/utils/ExceptionUtils.java
@@ -1,17 +1,7 @@
 package io.opentracing.contrib.annotation.utils;
 
 
-import java.util.function.Supplier;
-
 public class ExceptionUtils {
-
-    public static <T> T safeEx(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (Throwable e) {
-            return null;
-        }
-    }
 
     public static <T> T safeCheckEx(SupplierCheck<T> supplier) {
         try {
